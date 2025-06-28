1920fd7b9bf62f2286ddac5fa2de07756f11d07e
Luna Alkufairy <lunaalkufairy@gmail.com>
2025-06-28 17:21:49 +0300

    Editing Colors

--- lib/constants.dart
+++ lib/constants.dart
@@ -14,8 +14,8 @@ const Color black = Color(
     0xFF0B0B0C); //background color of the phone number and OTP text field, arrows and search text field  background color,
 const Color lightBlack = Color(
     0xFF252328); //Selection field, settings button, شروط والأحكام سياسة الخصوصية,
-const Color lightWhite1 =
-    Color(0xFFE1E1E1); //Phone number text field input and ordinary text
+
+const Color grey = Color(0xFF413D48); //text field border color.
 
 /*
 --------------------------------------------------------------------------------
@@ -25,7 +25,7 @@ const Color lightWhite1 =
 const Color lightGrey =
     Color(0xFFE4E4E7); //trip info card, location trip, map places, copon,
 const Color white = Color(
-    0xFFFFFFFF); //background color of the phone number and OTP and selection text field, Selection field, settings button, شروط والأحكام سياسة الخصوصية, arrows and search text field  background color,
+    0xFFFFFFFF); //background color of the phone number and OTP and selection text fields, text field background, settings button, شروط والأحكام سياسة الخصوصية, arrows and search text field  background color,ordinary text in DARK MODE
 
 /*
 --------------------------------------------------------------------------------
@@ -35,4 +35,6 @@ const Color white = Color(
 const Color lightWhite = Color(
     0xFFF5F2FB); // text field input in DARK MODE,text field border in LIGHT MODE,
 const Color grey1 = Color(
-    0xFF1E1E1E); //background color of text field, location trip, map places, copon in DARK MODE, text field input and ordinary text in LIGHT MODE,
+    0xFF1E1E1E); //background color of text field, location trip, map places, copon , download button in DARK MODE, text field input and ordinary text in LIGHT MODE,
+const Color lightWhite1 = Color(
+    0xFFE1E1E1); //Phone number text field input and ordinary text in DARK MODE, trip info card, location trip, map places, copon, download button in LIGHT MODE
