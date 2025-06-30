8be83b7b85c130d2146edc9ced4df5b693edc2ef
Luna Alkufairy <lunaalkufairy@gmail.com>
2025-06-30 14:47:15 +0300

    editing time picker

--- lib/components/custom_time_picker.dart
+++ lib/components/custom_time_picker.dart
@@ -14,6 +14,49 @@ class _CustomTimePickerState extends State<CustomTimePicker> {
   void _showTimePicker() {
     showTimePicker(
       context: context,
+      cancelText: 'إلغاء',
+      confirmText: 'متابعة',
+      helpText: 'تحديد الوقت',
+      builder: (BuildContext context, Widget? child) {
+        return Theme(
+          data: Theme.of(context).copyWith(
+            timePickerTheme: TimePickerThemeData(
+              backgroundColor: black1,
+              cancelButtonStyle: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(color: lightWhite1),
+                  borderRadius: BorderRadiusGeometry.circular(20),
+                ),
+                backgroundColor: white,
+                textStyle: TextStyle(
+                  fontFamily: 'assets/fonts/Cairo-Bold.ttf',
+                  fontSize: 20,
+                  color: grey1,
+                ),
+              ),
+              confirmButtonStyle: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(color: blue),
+                  borderRadius: BorderRadiusGeometry.circular(20),
+                ),
+                backgroundColor: blue,
+                textStyle: TextStyle(
+                  fontFamily: 'assets/fonts/Cairo-Bold.ttf',
+                  fontSize: 20,
+                  color: grey1,
+                ),
+              ),
+            ),
+          ),
+          child: Directionality(
+            textDirection: TextDirection.rtl,
+            child: child!,
+          ),
+        );
+      },
+      initialEntryMode: TimePickerEntryMode.dialOnly,
       initialTime: TimeOfDay.now(),
     ).then(
       (value) {
@@ -29,40 +72,41 @@ class _CustomTimePickerState extends State<CustomTimePicker> {
   @override
   Widget build(BuildContext context) {
     return TextButton(
-        onPressed: _showTimePicker,
-        style: TextButton.styleFrom(
-          backgroundColor: black1,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadiusGeometry.circular(15),
-            side: BorderSide(
-              color: lightWhite,
-            ),
+      onPressed: _showTimePicker,
+      style: TextButton.styleFrom(
+        backgroundColor: black1,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadiusGeometry.circular(15),
+          side: BorderSide(
+            color: lightWhite,
           ),
         ),
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Text(
-              textAlign: TextAlign.right,
-              textDirection: TextDirection.rtl,
-              timeOfDay.format(context).toString(),
-              style: TextStyle(
-                fontFamily: 'assets/fonts/Cairo-Bold.ttf',
-                color: white,
-              ),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Text(
+            textAlign: TextAlign.right,
+            textDirection: TextDirection.rtl,
+            timeOfDay.format(context).toString(),
+            style: TextStyle(
+              fontFamily: 'assets/fonts/Cairo-Bold.ttf',
+              color: white,
             ),
-            SizedBox(
-              width: 10,
-            ),
-            Container(
-                height: 32,
-                width: 32,
-                padding: EdgeInsets.all(2),
-                decoration: BoxDecoration(
-                    color: blue, borderRadius: BorderRadius.circular(10)),
-                child: SvgPicture.asset('assets/icons/Clock Circle.svg')),
-          ],
-        ));
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Container(
+              height: 32,
+              width: 32,
+              padding: EdgeInsets.all(2),
+              decoration: BoxDecoration(
+                  color: blue, borderRadius: BorderRadius.circular(10)),
+              child: SvgPicture.asset('assets/icons/Clock Circle.svg')),
+        ],
+      ),
+    );
   }
 }
