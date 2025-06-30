3a9781b25b630f7d780f06a23ab633509d8e104e
Luna Alkufairy <lunaalkufairy@gmail.com>
2025-06-30 12:11:42 +0300

    Editing arrow component

--- lib/components/custom_arrow.dart
+++ lib/components/custom_arrow.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:waslny_rider/constants.dart';
 
 class CustomArrow extends StatelessWidget {
-  const CustomArrow({super.key});
+  final VoidCallback onPressed;
+  const CustomArrow({super.key, required this.onPressed});
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () {},
+      onPressed: onPressed,
       style: ElevatedButton.styleFrom(
         shape: const CircleBorder(),
         padding: const EdgeInsets.all(12),
